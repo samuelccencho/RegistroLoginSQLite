283cc2173d7e05140ccb2dfc0548f76b4dcb6e7c
davjos7 <josephdavilahorna@gmail.com>
2025-05-21 19:50:19 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RegistroLoginSQLite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
